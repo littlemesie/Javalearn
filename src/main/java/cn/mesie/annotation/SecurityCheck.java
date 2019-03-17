4d6d049cede48ad510b18cca47b3c7a238f4c4dc
littlemesie <littlemesie@163.com>
2019-03-17 20:12:12 +0800

    annotation

---
+++ src/main/java/cn/mesie/annotation/SecurityCheck.java
@@ -0,0 +1,25 @@
+package cn.mesie.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by 2019-03-17 20:06
+ * 自定义@interface
+ * @author: mesie
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SecurityCheck {
+
+    boolean open() default false;
+
+    boolean justLimitFailure() default false;
+
+    String rule() default "";
+
+}
+
+
---
+++ src/main/java/cn/mesie/annotation/readme.md
@@ -0,0 +1,2 @@
+### 自定义注解
+- [参考](https://blog.csdn.net/qq_35625303/article/details/79403820)
